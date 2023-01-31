144f7ba06253d5af78a22dcdaaa693940fd46288
UT-T <1026071@gmail.com>
2023-02-01 08:20:40 +0900

    htmlを参照できていなかったため修正

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,15 @@ import jQuery = require('jquery');
 
 const app: Express = express()
 const port = process.env.PORT || 3000
+const html = '/work/out/html/'
 
 app.get('/', (req: Request, res: Response) => {
-    res.sendFile(__dirname + 'html/index.html');
+    res.sendFile(html+'index.html');
     // res.send('Hello')
 })
 
 app.get('/test', (req: Request, res: Response) => {
-    res.send('test')
+    res.sendFile(html+'test.html');
 })
 
 app.listen(port, () => {
